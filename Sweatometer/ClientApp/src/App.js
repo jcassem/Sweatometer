56a2ff3997bcc7b7eae222d252caa274f8b77d81
Jordan Cassem <cassem.codes@gmail.com>
2020-02-09 20:33:46 +0000

    Remove demo files.

--- Sweatometer/ClientApp/src/App.js
+++ Sweatometer/ClientApp/src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
 import { Sweatometer } from './components/Sweatometer';
-import { Counter } from './components/Counter';
 
 import './custom.css'
 
@@ -15,8 +13,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/sweatometer' component={Sweatometer} />
       </Layout>
     );
--- Sweatometer/ClientApp/src/components/NavMenu.js
+++ Sweatometer/ClientApp/src/components/NavMenu.js
@@ -31,13 +31,16 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                    <NavLink tag={Link} className="text-dark" to="/">Find</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                    <NavLink tag={Link} className="text-dark" to="/">Match</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                    <NavLink tag={Link} className="text-dark" to="/">Test</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/">About</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
