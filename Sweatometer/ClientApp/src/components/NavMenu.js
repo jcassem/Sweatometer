f5e91bddc6606d55fc1b00486f338d17244ebb56
Jordan Cassem <cassem.codes@gmail.com>
2020-02-21 14:49:42 +0000

    Add logo and favicon

--- Sweatometer/ClientApp/src/components/NavMenu.js
+++ Sweatometer/ClientApp/src/components/NavMenu.js
@@ -26,7 +26,9 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">Sweatometer</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">
+                <img src="/logo.png" class="navbar-logo" /> Sweatometer
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
