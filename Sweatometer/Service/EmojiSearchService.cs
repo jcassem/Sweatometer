55f369458c40cec4714c763c2ea5adee0da92c8c
Jordan Cassem <cassem.codes@gmail.com>
2020-04-29 20:54:56 +0100

    Ignore word order when looking for emoji search terms

--- Sweatometer/Service/EmojiSearchService.cs
+++ Sweatometer/Service/EmojiSearchService.cs
@@ -18,7 +18,7 @@ namespace Sweatometer
             var keys = EmojiLoader.EmojiDictionary.Keys;
 
             await Task.Run(() => {
-                var matchedKeys = keys.Where(key => WholeWordSearch(key, searchWord));
+                var matchedKeys = keys.Where(key => ContainsAllWords(key, searchWord));
 
                 if (matchedKeys.Any())
                 {
@@ -45,9 +45,33 @@ namespace Sweatometer
             return emojis;
         }
 
-        public static bool WholeWordSearch(string input, string searchTerm){
-            var pattern = @"\b" + Regex.Escape(searchTerm.ToLower()) + @"\b";
-            return Regex.IsMatch(input.ToLower(), pattern); 
+        /// <summary>
+        /// Searches <paramref name="toSearchIn"/> for all the words in <paramref name="toSearchFor"/> in any order (case in-sensitive).
+        /// </summary>
+        /// <param name="toSearchIn">String to search for words in.</param>
+        /// <param name="toSearchFor">String to search for.<toSearchIn></param>
+        /// <returns>Whether <paramref name="toSearchIn"/> contains all the words in <paramref name="toSearchFor"/>.</returns>
+        public static bool ContainsAllWords(string toSearchIn, string toSearchFor){
+            if(!WholeWordSearch(toSearchIn, toSearchFor)){
+                foreach(String word in toSearchFor.Split(' ')){
+                    if(!WholeWordSearch(toSearchIn, word)){
+                        return false;
+                    }
+                }
+            }
+                
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if <paramref name="toSearchIn"/> contains <paramref name="toSearchFor"/> (ordered match).
+        /// </summary>
+        /// <param name="toSearchIn">String to search in.</param>
+        /// <param name="toSearchFor">String to search for.</param>
+        /// <returns>Whether <paramref name="toSearchIn"/> contains <paramref name="toSearchFor"/>.</returns>
+        public static bool WholeWordSearch(string toSearchIn, string toSearchFor){
+            var pattern = @"\b" + Regex.Escape(toSearchFor.ToLower()) + @"\b";
+            return Regex.IsMatch(toSearchIn.ToLower(), pattern);
         }
     }
 }
